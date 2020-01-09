e0ffc103fac9342f77a8a5979d2d378382e6c747
musupadi159@gmail.com <99162262s>
2020-01-09 07:01:42 +0700

    Initial commit

---
+++ app/src/main/java/com/destinyapp/biodatawalisongo/Activity/MainActivity.java
@@ -0,0 +1,16 @@
+package com.destinyapp.biodatawalisongo.Activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.destinyapp.biodatawalisongo.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
