709e04b6641167ca4d9a321f0b431889c7eb0755
Muhammad Supriyadi <musupadi159@gmail.com>
2020-01-10 09:33:41 +0700

    ss

---
+++ app/src/main/java/com/destinyapp/biodatawalisongo/Activity/BiodataActivity.java
@@ -0,0 +1,16 @@
+package com.destinyapp.biodatawalisongo.Activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.destinyapp.biodatawalisongo.R;
+
+public class BiodataActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_biodata);
+    }
+}
