bd20c876f5f1eeed92124ce59525726fc40654a3
musupadi159@gmail.com <99162262s>
2020-01-11 15:52:02 +0700

    ss

---
+++ app/src/main/java/com/destinyapp/biodatawalisongo/Activity/TentangActivity.java
@@ -0,0 +1,44 @@
+package com.destinyapp.biodatawalisongo.Activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.drawable.Drawable;
+import android.media.MediaPlayer;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
+import com.destinyapp.biodatawalisongo.R;
+
+public class TentangActivity extends AppCompatActivity {
+    boolean onClicked = true;
+    ImageView kembali,play;
+    MediaPlayer SuaraLagu;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tentang);
+        play=(ImageView)findViewById(R.id.ivMusikPlay);
+
+        SuaraLagu = MediaPlayer.create(TentangActivity.this,R.raw.about);
+
+        play.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                final Drawable DrawableHapus = getResources().getDrawable(R.drawable.pause);
+                final Drawable DrawablePlay = getResources().getDrawable(R.drawable.play);
+                if (onClicked){
+                    play.setImageDrawable(DrawableHapus);
+                    onClicked=false;
+                    SuaraLagu.start();
+                }else{
+                    play.setImageDrawable(DrawablePlay);
+                    onClicked=true;
+                    SuaraLagu.pause();
+                }
+            }
+        });
+    }
+
+
+}
